636c4bd80663b2fed96d43faaa2b9620cd152fb2
Niklas Anderson <nkanders@gmail.com>
2025-02-13 15:46:59 -0800

    Extend `debug_print` functionality Extends the `debug_print` functionality to accept variables.

--- libs/common/Inc/logging.h
+++ libs/common/Inc/logging.h
@@ -11,12 +11,28 @@
 
 #include "main.h"
 #include <string.h>
+#include <stdio.h> // Required for snprintf()
 
 #ifdef DEBUG
     #define DEBUG_UART huart2
-    #define debug_print(msg) do { \
-        HAL_UART_Transmit(&DEBUG_UART, (uint8_t*)(msg), strlen(msg), 100); \
-    } while(0)
+    #define debug_print(fmt, ...)                                                                                    \
+    do                                                                                                               \
+    {                                                                                                                \
+        char buffer[128]; /* Adjust size as needed */                                                                \
+        int result = snprintf(buffer, sizeof(buffer), fmt, ##__VA_ARGS__);                                           \
+        if (result < 0)                                                                                              \
+        {                                                                                                            \
+            /* Handle error */                                                                                       \
+            HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)"snprintf error\n", 15, 100);                                  \
+        }                                                                                                            \
+        else                                                                                                         \
+        {                                                                                                            \
+            /* Cast to size_t for the comparison to sizeof result below */                                           \
+            size_t len = (size_t)result;                                                                             \
+            buffer[sizeof(buffer) - 1] = '\0'; /* Explicit null termination */                                       \
+            HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)buffer, len > sizeof(buffer) ? sizeof(buffer) - 1 : len, 100); \
+        }                                                                                                            \
+    } while (0)
 #else
     #define debug_print(msg)
 #endif
